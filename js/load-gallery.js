34041166944ad5f33af24ed2fc23747408dc4810
ngowhere <91102796+ngowhere@users.noreply.github.com>
2022-04-28 13:03:28 -0400

    all traditional art added, proper title and descriptions given

--- js/load-gallery.js
+++ js/load-gallery.js
@@ -1,67 +1,84 @@
 console.log("------START-------");
-// Temporary file until I figure out how to load a JSON file
-trad = [   
-    "{\"title\": \"Celadon\", \"src\": \"imgs/trad/celadon.jpg\", \"description\": \"A portrait\", \"medium\": \"acrylic\"}", 
-    "{\"title\": \"The Magician\", \"src\": \"imgs/trad/magician.jpg\", \"description\": \"\", \"medium\": \"\"}",
-    "{\"title\": \"CutUpStars\", \"src\": \"imgs/trad/1_Ngo_CutUpStars.jpg\", \"description\": \"\", \"medium\": \"\"}",
-    "{\"title\": \"MemStreetGood\", \"src\": \"imgs/trad/MemStreetGood.jpg\", \"description\": \"\", \"medium\": \"\"}",
-    "{\"title\": \"ViewOfBullCreek\", \"src\": \"imgs/trad/2_Ngo_ViewOfBullCreek.jpg\", \"description\": \"\", \"medium\": \"\"}",
-    "{\"title\": \"haroldFinal\", \"src\": \"imgs/trad/haroldFinal.JPG\", \"description\": \"\", \"medium\": \"\"}",
-    "{\"title\": \"syndrome\", \"src\": \"imgs/trad/imposter_syndrome.JPG\", \"description\": \"\", \"medium\": \"\"}",
-    "{\"title\": \"musicboi2\", \"src\": \"imgs/trad/musicboi2.JPG\", \"description\": \"\", \"medium\": \"\"}"
-]
+// Call only to refresh galleries
+// localStorage.clear();
 
 
 // Load JSON data into array
-traditional = []
-for (let i = 0; i < trad.length; i++){
-    traditional[i] = JSON.parse(trad[i]);
-    // console.log(traditional[i].title);
-}
+var traditional = []
+var all = []
+var digital = []
 
-modes = {
-    "trad" : traditional
+
+traditional =  loadObjArray(trad)
+var modes = {
+    "trad" : traditional, 
+    "all" : all, 
+    "digital" : digital
 }
 
 function setUp(){
     console.log("setting up");
     if (localStorage.getItem('allIndex') == null){
+        console.log("null");
         localStorage.setItem('allIndex', 0);
         localStorage.setItem('tradIndex', 0);
         localStorage.setItem('digIndex', 0);
         // Change default mode to all 
         localStorage.setItem('mode', 'trad');
+
     }
 
+}
+
+function loadObjArray(arr){
+    let result = []
+    for (let i = 0; i < arr.length; i++){
+        result[i] = JSON.parse(arr[i]);
+    }
+    return result
 }
 
 function showGalleryImages(){
     setUp()
     console.log('SHOWING GALLERY IMAGES');
     const gallery = document.getElementById("gallery");
-    console.log(gallery);
+    removeAllChildNodes(gallery);
 
-    for (let i = 0; i < traditional.length; i++){
-        var link = document.createElement("a");
-        // link.href = "slide.html"
-        var img = document.createElement("img");
-        img.src = traditional[i].src;
-        img.classList.add("img-fluid");
-        img.classList.add("img-gallery");
-        img.id = i
-      
-        link.appendChild(img);
-        gallery.appendChild(link);
+    current = getCurr();
+    console.log(current);
 
-        // set image index in before going to slide mode when img clicked
-        img.addEventListener('click', function (e) {
-            index = e.target.id
-            console.log(index);
-            current = getCurr()
-            localStorage.setItem(current.modeIndexName, index);
-            location.href = "slide.html"
-        });
+    if (current.array == null){
+        console.log("Nothing");
+        var p = document.createElement("p");
+        var text = document.createTextNode("Currently no projects of this type at this time!");
+        p.appendChild(text);
+        gallery.appendChild(p);
+    }else{
+        for (let i = 0; i < current.array.length; i++){
+            var link = document.createElement("a");
+            // link.href = "slide.html"
+            var img = document.createElement("img");
+            img.src = current.array[i].src;
+            img.classList.add("img-fluid");
+            img.classList.add("img-gallery");
+            img.id = i
+        
+            link.appendChild(img);
+            gallery.appendChild(link);
+
+            // set image index in before going to slide mode when img clicked
+            img.addEventListener('click', function (e) {
+                index = e.target.id
+                console.log(index);
+                current = getCurr()
+                localStorage.setItem(current.modeIndexName, index);
+                location.href = "slide.html"
+            });
+        }
+
     }
+
+        
 }
 
 //set slide information 
@@ -75,6 +92,7 @@ function showSlide(){
 function getCurr(){
     let m = localStorage.getItem('mode');
     let currArr = modes[m];
+    console.log(modes)
     let index = localStorage.getItem(m + 'Index');
 
     return{
@@ -123,4 +141,23 @@ function doWrapAround(index, arr){
     }
 }
 
+function setMode(modeName){
+    localStorage.setItem('mode', modeName);
+    console.log(localStorage.getItem('mode'));
+    // Code to bold the name of the current mode
+    if (modeName == "trad"){
 
+    }else if (modeName == "dig"){
+
+    }else{
+
+    }
+    showGalleryImages()
+}
+
+// Removes all children from a node
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
